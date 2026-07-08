9b39b6594ef12aa0fda0c74f8fd3973c83efa867
Darkest-Night-427 <darkestnight427@gmail.com>
2026-07-08 18:59:32 -0400

    Update core.js

--- JS/core.js
+++ JS/core.js
@@ -1,1 +1,95 @@
+// NightShadeOS Core Engine
+// Version: v0.0.1-alpha
 
+
+const bootMessages = [
+
+    "Initializing Core...",
+
+    "Loading Sanctum...",
+
+    "Preparing Applications...",
+
+    "Establishing Astral Services...",
+
+    "Checking Memory...",
+
+    "Preparing Launcher...",
+
+    "Ready."
+
+];
+
+
+let messageIndex = 0;
+
+let progress = 0;
+
+
+const messageBox = document.getElementById(
+    "boot-message"
+);
+
+const progressBar = document.getElementById(
+    "progress"
+);
+
+
+
+function updateBoot(){
+
+    if(messageIndex < bootMessages.length){
+
+        messageBox.textContent =
+        bootMessages[messageIndex];
+
+
+        messageIndex++;
+
+    }
+
+
+    progress += 15;
+
+
+    progressBar.style.width =
+    progress + "%";
+
+
+
+    if(progress >= 100){
+
+        finishBoot();
+
+    }
+
+}
+
+
+
+function finishBoot(){
+
+    messageBox.textContent =
+    "Welcome to NightShadeOS";
+
+
+    setTimeout(()=>{
+
+        document.getElementById(
+            "boot-screen"
+        ).style.opacity = "0";
+
+
+    },1000);
+
+}
+
+
+
+setInterval(
+
+    updateBoot,
+
+    900
+
+);
