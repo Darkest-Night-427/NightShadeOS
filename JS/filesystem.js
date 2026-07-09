ea336e5294682421f1594d12105cd2b514da9525
Darkest-Night-427 <darkestnight427@gmail.com>
2026-07-09 16:36:11 -0400

    Create filesystem.js

---
+++ JS/filesystem.js
@@ -0,0 +1,28 @@
+// NightShadeOS Virtual File System
+
+const fileSystem = {
+    "/": {
+        folders: ["Documents", "Downloads", "Pictures", "Music"],
+        files: ["Welcome.txt"]
+    },
+
+    "/Documents": {
+        folders: [],
+        files: ["Homework.doc", "Notes.txt"]
+    },
+
+    "/Downloads": {
+        folders: [],
+        files: ["Installer.exe"]
+    },
+
+    "/Pictures": {
+        folders: [],
+        files: ["Moon.png"]
+    },
+
+    "/Music": {
+        folders: [],
+        files: []
+    }
+};
