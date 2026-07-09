c98ea672bb579f2fa5abd87986bed29911b448fd
Darkest-Night-427 <darkestnight427@gmail.com>
2026-07-08 20:38:26 -0400

    Update windows.js

--- JS/windows.js
+++ JS/windows.js
@@ -1,1 +1,113 @@
+// NightShadeOS Window Core
+// Version: v0.1-alpha
 
+
+let windowCount = 0;
+
+
+function createWindow(title, content){
+
+
+    windowCount++;
+
+
+    const win =
+    document.createElement("div");
+
+
+    win.className =
+    "ns-window";
+
+
+    win.id =
+    "window-" + windowCount;
+
+
+
+    win.innerHTML = `
+
+        <div class="window-header">
+
+            <span>
+                ${title}
+            </span>
+
+
+            <div class="window-buttons">
+
+                <button class="minimize">
+                    —
+                </button>
+
+
+                <button class="maximize">
+                    □
+                </button>
+
+
+                <button class="close">
+                    X
+                </button>
+
+            </div>
+
+        </div>
+
+
+        <div class="window-content">
+
+            ${content}
+
+        </div>
+
+    `;
+
+
+
+    document
+    .getElementById("sanctum")
+    .appendChild(win);
+
+
+
+    setupWindow(win);
+
+
+}
+
+
+
+function setupWindow(win){
+
+
+    win.querySelector(".close")
+    .onclick = ()=>{
+
+        win.remove();
+
+    };
+
+
+
+    win.querySelector(".minimize")
+    .onclick = ()=>{
+
+        win.style.display =
+        "none";
+
+    };
+
+
+
+    win.querySelector(".maximize")
+    .onclick = ()=>{
+
+
+        win.classList.toggle(
+            "maximized"
+        );
+
+
+    };
+
+}
